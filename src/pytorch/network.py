4b030de9b9430f67afc3e63b325285c673da4d34
Wong Fu Jie <wongfujie@u.nus.edu>
2022-02-18 12:10:54 +0800

    Corrected Pylint messages in pytorch/network.py

--- src/pytorch/network.py
+++ src/pytorch/network.py
@@ -1,8 +1,30 @@
+"""This module contains the default pytorch neural network model for MNIST"""
+
 import torch.nn as nn
-import torch.nn.functional as F
+import torch.nn.functional as fn
 
 
 class MNISTNet(nn.Module):
+    """
+    Neural network model for classifying MNIST dataset
+    '''
+    Attributes
+    ----------
+    conv1: Conv2d
+        1st 2D convolution layer
+    conv2: Conv2d
+        2nd 2D convolution layer
+    fc1: Linear
+        1st linear layer
+    fc2: Linear
+        2nd linear layer
+
+    Methods
+    -------
+    forward(x=object)
+        Constructs the neural network.
+    """
+
     def __init__(self):
         super(MNISTNet, self).__init__()
         self.conv1 = nn.Conv2d(1, 20, 5, 1)
@@ -11,11 +33,17 @@ class MNISTNet(nn.Module):
         self.fc2 = nn.Linear(500, 10)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.max_pool2d(x, 2, 2)
-        x = F.relu(self.conv2(x))
-        x = F.max_pool2d(x, 2, 2)
+        """Constructs the neural network.
+        Parameters
+        ----------
+        x: object
+            Holds the built network construct
+        """
+        x = fn.relu(self.conv1(x))
+        x = fn.max_pool2d(x, 2, 2)
+        x = fn.relu(self.conv2(x))
+        x = fn.max_pool2d(x, 2, 2)
         x = x.view(-1, 4 * 4 * 50)
-        x = F.relu(self.fc1(x))
+        x = fn.relu(self.fc1(x))
         x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
+        return fn.log_softmax(x, dim=1)
