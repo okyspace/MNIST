485bf6552ab393dd05d8937096785e7283de625d
Wong Fu Jie <wongfujie@u.nus.edu>
2022-02-25 17:48:13 +0800

    Updated utils/utils_triton.py script to be default pylint accepted. This script is WIP and still has TODO warnings.

--- src/utils/utils_triton.py
+++ src/utils/utils_triton.py
@@ -1,5 +1,10 @@
-# to extend with more config possibilities
+"""This module contains utility codes for interacting with Triton server"""
+# TODO: To extend with more config possibilities
 def create_config_pbtxt_tensorflow(model, config_pbtxt_file):
+    """
+    DOCSTRING TO BE INCLUDED!
+    """
+
     platform = "tensorflow_savedmodel"
     input_name = model.input_names[0]
     output_name = model.output_names[0]
@@ -8,8 +13,9 @@ def create_config_pbtxt_tensorflow(model, config_pbtxt_file):
     input_dims = str(model.input.shape.as_list()).replace("None", "-1")
     output_dims = str(model.output.shape.as_list()).replace("None", "-1")
 
+    # String is meant to look like a file and is simpler to write using literals.
+    # pylint: disable-next=consider-using-f-string
     config_pbtxt = """
-  
         platform: "%s"
         input [
             {
@@ -26,24 +32,40 @@ def create_config_pbtxt_tensorflow(model, config_pbtxt_file):
             }
         ]
     """ % (
-      
         platform,
-        input_name, input_data_type, input_dims,
-        output_name, output_data_type, output_dims
+        input_name,
+        input_data_type,
+        input_dims,
+        output_name,
+        output_data_type,
+        output_dims,
     )
 
-    with open(config_pbtxt_file, "w") as config_file:
+    with open(config_pbtxt_file, "w", encoding="utf-8") as config_file:
         config_file.write(config_pbtxt)
 
 
-# add in input format for sample.
-def convert2torchscript(sample, model):
-    # TODO
+# TODO: Add in input format for sample.
+def convert2torchscript():
+    """
+    DOCSTRING TO BE INCLUDED!
+    Parameters (Not included in function yet due to non reference)
+    ----------
+    sample:
+    model:
+    """
 
 
-# to extend with more config possibilities (dyanmic batching, model instances, versions) and variable inputs/outputs
-# incomplete; todo
-def create_config_pbtxt_torchscript(model, config_pbtxt_file):
-    platform = "pytorch_libtorch"
-    input_name = 'input__0'
-    output_name = 'output__0'
+# TODO: To extend with more config possibilities (dyanmic batching, model instances, versions)
+# and variable inputs/outputs
+def create_config_pbtxt_torchscript():
+    """
+    DOCSTRING TO BE INCLUDED!
+    Parameters (Not included in function yet due to non reference)
+    ----------
+    model:
+    config_pbtxt_file:
+    """
+    # platform = "pytorch_libtorch"
+    # input_name = "input__0"
+    # output_name = "output__0"
